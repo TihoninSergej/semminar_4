c26f45346bb0d2c41a93d14e0cd40005f1273948
Sergei Tihonin <tihonin.serega@mail.ru>
2022-11-27 17:20:19 +0300

    задача29

---
+++ task29/Program.cs
@@ -0,0 +1,26 @@
+﻿int [] GetRange( int Length)
+{
+    int [] array = new int [Length];
+    Random rd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rd.Next(0, 99);
+    }
+    return array;
+}
+
+void PrintArray( int [] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+int [] array = GetRange(8);
+PrintArray(array);
